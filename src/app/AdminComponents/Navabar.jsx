5cc0baf4d5b72a82bd7be77db7fb713a8daa33c9
Abinesh J <abineshleyon@gmail.com>
2023-12-05 21:13:06 +0530

    home page setup

--- src/app/AdminComponents/Navabar.jsx
+++ src/app/AdminComponents/Navabar.jsx
@@ -18,14 +18,15 @@ const Navabar = ({toggle ,funtoggle,mfuntoggle,mtoggle}) => {
       <div className="flex justify-center items-center">
       <HiMiniBars3 className='mr-2 md:block hidden' style={{ fontSize: "25px" }} onClick={()=>funtoggle(!toggle)}/>
       <HiMiniBars3 className='mr-2 md:hidden' style={{ fontSize: "25px" }} onClick={changevalue}/>
-        <Image src='https://i.postimg.cc/XYDxVftd/Whats-App-Image-2023-11-25-at-8-30-15-PM.jpg' alt="image" style={{ width: "100px" }} />
+      Abitty Retails
+        {/* <Image src='https://i.postimg.cc/XYDxVftd/Whats-App-Image-2023-11-25-at-8-30-15-PM.jpg' alt="image"  width= "100" /> */}
       </div>
       <div className='md:block hidden'>
         <input type="text" placeholder='search' className="border border-gray-300  rounded-lg focus:outline-slate-400 " autoComplete='off' style={{ width: "500px", padding: "6px" }} />
       </div>
       <div className="flex items-center justify-center gap-4">
         <PiBell style={{ fontSize: "20px" }} />
-        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png" alt="image" style={{ width: "30px", height: "30px", borderRadius: "50%" }} className="border-cyan-950" />
+        {/* <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png" alt="image" style={{ width: "30px", height: "30px", borderRadius: "50%" }} className="border-cyan-950" /> */}
         <p className='md:block hidden'>Abinesh J</p>
         <PiCaretUpDown style={{ fontSize: "20px" }} />
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className=" w-screen h-screen flex justify-center items-center">
+      <Link href="/admin/dashboard" className="p-3 bg-slate-800 rounded text-white">Admin dashboard</Link>
     </main>
   )
 }
