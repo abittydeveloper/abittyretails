29384425e1f2c0e3197a622e0f742baea2d1df3d
abittydeveloper <138377667+abittydeveloper@users.noreply.github.com>
2024-06-13 15:07:15 +0530

    google

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   const pdfUrl = "https://intech-review3lts.rivervalleytechnologies.in/storage/uploads/acrt/RESP/1254/acrt-RESP-1718023022.pdf"
+  const googleDocsViewerUrl = `https://docs.google.com/viewer?url=${encodeURIComponent(pdfUrl)}&embedded=true`;
   return (
     <main className=" w-screen h-screen flex justify-center items-center">
     <div>Hello from abitty</div>
       <iframe
-        src={pdfUrl}
+        src={googleDocsViewerUrl}
         width="100%"
         height="750px"
         style={{ border: 'none' }}
