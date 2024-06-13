d9325b8d88262cd8bbbc0afab88c72bec7aca742
abittydeveloper <138377667+abittydeveloper@users.noreply.github.com>
2024-06-13 14:54:49 +0530

    Emmeded

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,16 @@ import Link from "next/link";
 
 
 export default function Home() {
+  const pdfUrl = "https://intech-review3lts.rivervalleytechnologies.in/storage/uploads/acrt/RESP/1254/acrt-RESP-1718023022.pdf"
   return (
     <main className=" w-screen h-screen flex justify-center items-center">
-      <Link href="/admin/dashboard" className="p-3 bg-slate-800 rounded text-white">Admin dashboard</Link>
+      <embed
+        src={pdfUrl}
+        width="100%"
+        height="750px"
+        type="application/pdf"
+        style={{ border: 'none' }}
+      />
     </main>
   )
 }
